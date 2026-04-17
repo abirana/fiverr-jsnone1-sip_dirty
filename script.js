7c858ca76e5b9c6a3aca4fe5181f672f06f0eade
Abi Rana <info@abirana.com>
2026-04-17 23:40:54 +0545

    easing motion refinements

--- script.js
+++ script.js
@@ -21,17 +21,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
   gsap.set(vanTyres, { transformOrigin: "center" });
 
   function introAnimation() {
-    const tl = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
+    const tl = gsap.timeline();
     
     tl
-      .to(van, { right: "70%", duration: 1 })
+      .to(van, { right: "70%", duration: 1.5, ease: "power1.out" })
+      .to(vanTyres, { rotation: "-=1080", duration: 1.5, ease: "power1.out" }, "<")
       .to(van, { rotationY: 180, duration: 0 })
-      .to(van, { right: "-100%", bottom: "40%", duration: 1.5, scale: 0.5 })
-      .to(van, { rotationY: 0, duration: 0 })
-      .to(van, { right: "0%", bottom: "50%", duration: 1.5, scale: 1 })
-      .to(vanTyres, { rotation: "-=360", repeat: 2.75, ease: "none" }, 0)
-      .to(van, {scale: 5, transformOrigin: "76% 22.5%", ease: "power2.in"}, "+=0.5")
-      .to(introSection, { autoAlpha: 0 })
+      .to(van, { right: "-100%", bottom: "40%", duration: 1.5, scale: 0.5, ease: "power2.in" })
+      .to(vanTyres, { rotation: "-=1080", duration: 1.5, ease: "power2.out" }, "<")
+      .to(van, { rotationY: 0, bottom: "50%", scale: 0.8, duration: 0 })
+      .to(van, { right: "0%", duration: 1.5, scale: 1, ease: "back.out(0.55)" }, "+=1")
+      .to(vanTyres, { rotation: "-=720", duration: 1.5, ease: "back.out(0.55)" }, "<")
+      .to(van, { rotation: 0.25, duration: 0.5, ease: "power1.in" }, ">-0.5")
+      .to(van, { rotation: 0, duration: 1, ease: "power1.out" }, ">")
+      // .to(van, {scale: 5, transformOrigin: "76% 22.5%", duration: 2, ease: "power2.inOut"}, "-=0.5")
+      // .to(introSection, { duration: 1.5, autoAlpha: 0 }, "+=0.25")
+      .to(van, {scale: 7.5, transformOrigin: "73.75% 25%", duration: 2, ease: "power2.inOut"}, "-=0.5")
+      .to(introSection, { duration: 0.75, autoAlpha: 0 }, "<+1")
       .to("body", { overflow: "" });
 
     return tl;
